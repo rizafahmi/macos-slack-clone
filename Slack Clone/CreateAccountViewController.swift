565713e3332a22f3e25f1d7853ba8d3047f4898e
Riza Fahmi <rizafahmi@gmail.com>
2017-12-24 18:14:43 +0700

    Add CreateAccountViewController

---
+++ Slack Clone/CreateAccountViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAccountViewController.swift
+//  Slack Clone
+//
+//  Created by Riza Fahmi on 12/24/17.
+//  Copyright © 2017 hacktiv8. All rights reserved.
+//
+
+import Cocoa
+
+class CreateAccountViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
