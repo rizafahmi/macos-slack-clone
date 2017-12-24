becc2bba5694b131636640f6e478b9e64c6541f9
Riza Fahmi <rizafahmi@gmail.com>
2017-12-24 17:55:36 +0700

    Initial Commit

---
+++ Slack Clone/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Slack Clone
+//
+//  Created by Riza Fahmi on 12/24/17.
+//  Copyright © 2017 hacktiv8. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Slack Clone/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Slack Clone
+//
+//  Created by Riza Fahmi on 12/24/17.
+//  Copyright © 2017 hacktiv8. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
