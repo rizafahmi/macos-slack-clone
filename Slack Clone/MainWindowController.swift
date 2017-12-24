dc28e0c3e9ab0e6b0e25a2180b5a810406574cc0
Riza Fahmi <rizafahmi@gmail.com>
2017-12-24 18:13:13 +0700

    Attach MainWindowController

---
+++ Slack Clone/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  Slack Clone
+//
+//  Created by Riza Fahmi on 12/24/17.
+//  Copyright © 2017 hacktiv8. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
